3b4088ba6046fd308bea8aaf1058fbb9ae0b45ab
oumaymabg <oumaymabou257@gmail.com>
2021-07-25 23:33:24 +0200

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+Module that takes two strings and returns a concatenated one
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Type-annotated function that takes two strings and returns a concatenated
+    one
+    @str1: the first string
+    @str2: the second string
+    Return: a concatenated string containing the teo strings
+    '''
+    return str1 + str2
