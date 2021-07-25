18ec67801b6e80037527cd5238182d1e2977fe10
oumaymabg <oumaymabou257@gmail.com>
2021-07-25 23:41:53 +0200

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Module that defines several type-annotated variables
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
