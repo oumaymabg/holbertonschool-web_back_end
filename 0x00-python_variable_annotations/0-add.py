4a871e76b3e341d9e4331786ca66378de8f84257
oumaymabg <oumaymabou257@gmail.com>
2021-07-25 23:30:07 +0200

    0-add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Module that adds two floats and returns the sum as float'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Type-annotated function that takes two floats and returns the sum as float
+    @a: first float
+    @b: second float
+    Return: the sum represented as a float
+    '''
+    return a + b
