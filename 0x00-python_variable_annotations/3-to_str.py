f619ea00f0c3fd2ec4a4dc1fe5802098427810aa
oumaymabg <oumaymabou257@gmail.com>
2021-07-25 23:39:40 +0200

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Module that takes a float and returns the string representation of the float
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Type-annotated function that takes a float and returns the string
+    representation of the float
+    @n: float that will be returned as string
+    Return: string representation of n
+    '''
+    return str(n)
