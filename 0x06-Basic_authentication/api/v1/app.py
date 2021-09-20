145766848b4a6ea1398c44de882e1c1c36abb991
oumaymabg <oumaymabou257@gmail.com>
2021-09-20 23:01:50 +0200

    app

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -13,18 +13,17 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-@app.errorhandler(401)
-def not_found(error) -> str:
-    """ Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}),401
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def not_found(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}),401
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
