3d1ede50065ec1639700cd60406ebf9da5b60114
oumaymabg <oumaymabou257@gmail.com>
2021-11-07 23:39:41 +0100

    try

---
+++ 0x0F-ES6_promise/8-try.js
@@ -0,0 +1,5 @@
+//Throw error / try catch
+export default function divideFunction(numerator, denominator) {
+    if (denominator === 0) throw new Error('cannot divide by 0');
+    return numerator / denominator;
+  }
